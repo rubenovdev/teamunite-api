df44fdbc3c2cd14743ebe61c8b1deb747f000a88
drmrhdt <drmrhdt@gmail.com>
2020-03-21 15:14:07 +0300

    Add new user to cennection url

--- backend/server.js
+++ backend/server.js
@@ -10,25 +10,36 @@ const findDocuments = require("./queries").findDocuments;
 // const url = process.env.MONGO_DB_ADDRESS
 //   ? "mongodb://localhost:27017"
 //   : process.env.MONGO_DB_ADDRESS;
-const url =
-  "mongodb+srv://Daria:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
+// const url =
+//   "mongodb+srv://Daria:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
 
-// Database Name
-const dbName = "teamunite";
+// // Database Name
+// const dbName = "teamunite";
 
-// Create a new MongoClient
-const client = new MongoClient(url);
+// // Create a new MongoClient
+// const client = new MongoClient(url);
 
 // Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  listDatabases(client);
-  // const db = client.db(dbName);
+// client.connect(function(err) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+//   listDatabases(client);
+// const db = client.db(dbName);
 
-  // findDocuments(db, function() {
-  //   client.close();
-  // });
+// findDocuments(db, function() {
+//   client.close();
+// });
+// });
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://DariaPopova:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("teamunite").collection("projects");
+  console.log(collection);
+  // perform actions on the collection object
+  client.close();
 });
 
 const port = process.env.PORT || 5000;
