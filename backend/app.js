5cfd5b19f9be8629a9f085bc3c69a29e6aa7ff9a
drmrhdt <drmrhdt@gmail.com>
2020-03-21 18:42:51 +0300

    Update server.js with test data

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
-    results: projects.length,
+    results: "Im length",
     data: "Im data"
   });
 });
