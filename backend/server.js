8c95a9437555af36ec4bee3276ed2ad8f6776850
drmrhdt <drmrhdt@gmail.com>
2020-03-21 15:37:25 +0300

    Update test code for db in server.js
    Update server.js with test query to db

--- backend/server.js
+++ backend/server.js
@@ -31,15 +31,15 @@ const findDocuments = require("./queries").findDocuments;
 // });
 // });
 
-// const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://DariaPopova:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("teamunite").collection("projects");
-  console.log(collection);
-  // perform actions on the collection object
-  client.close();
+  console.log("Connected successfully to server");
+  const db = client.db("projects");
+  findDocuments(db, function() {
+    client.close();
+  });
 });
 
 const port = process.env.PORT || 5000;
