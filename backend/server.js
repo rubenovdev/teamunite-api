66cf2df8fab106d7b530b6b94c11d759cfef0cef
drmrhdt <drmrhdt@gmail.com>
2020-03-21 18:34:04 +0300

    Polish code, add mongoose connection
    Delete comments, local json files and test js files
    Create config.env file

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+dotenv.config({ path: "./config.env" });
 
-const insertProjects = require("./queries").insertProjects;
-const findDocuments = require("./queries").findDocuments;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-// Connection URL
-// const url = process.env.MONGO_DB_ADDRESS
-//   ? "mongodb://localhost:27017"
-//   : process.env.MONGO_DB_ADDRESS;
-// const url =
-//   "mongodb+srv://Daria:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
-
-// // Database Name
-// const dbName = "teamunite";
-
-// // Create a new MongoClient
-// const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-// client.connect(function(err) {
-//   assert.equal(null, err);
-//   console.log("Connected successfully to server");
-//   listDatabases(client);
-// const db = client.db(dbName);
-
-// findDocuments(db, function() {
-//   client.close();
-// });
-// });
-
-const uri =
-  "mongodb+srv://DariaPopova:12e9843Zi1iBipDye3ve6TUBv5@cluster0-8pzqh.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  console.log("Connected successfully to server");
-  const db = client.db("projects");
-  findDocuments(db, function() {
-    client.close();
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(connection => {
+    console.log(connection.connections);
+    console.log("Connected successfully to server");
   });
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
