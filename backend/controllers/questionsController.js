c77de4308acd24919516d0bcf69ce70bcc943ef7
drmrhdt <drmrhdt@gmail.com>
2020-04-05 19:46:35 +0300

    Update questionsController.js
    addQuestion awaits for array of answers instead of object of answers

--- backend/controllers/questionsController.js
+++ backend/controllers/questionsController.js
@@ -2,11 +2,19 @@ const Question = require("../models/questionModel");
 
 exports.addQuestion = async (req, res) => {
   try {
-    await Question.create(req.body);
+    const answersArr = req.body;
+    const answersObj = {};
+
+    answersArr.forEach((answer, index) => {
+      answersObj[`question${index + 1}`] = answer;
+    });
+
+    await Question.create(answersObj);
+
     res.status(200).json({
       status: "success",
       data: {
-        project: req.body,
+        answersArr,
       },
     });
   } catch (err) {
