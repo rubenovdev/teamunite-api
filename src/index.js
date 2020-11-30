4c412ba935a7b8c0ed26768440194ae4a688f83a
khotsevich <xozewitc@yandex.ru>
2020-11-30 13:46:08 +0300

    Добавил документацию

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import config from './config/config.js'
 const start = async () => {
   try {
     await mongodb()
-    server.listen(config.PORT, () =>
+    server.listen(process.env.PORT || config.PORT, () =>
       console.log(`Server is running on port ${config.PORT}`)
     )
   } catch (e) {
